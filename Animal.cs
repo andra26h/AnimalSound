fda627918c138f1c3f2ac079fdd8f6a50fa9e53b
andra.hadar <hadarandra@yahoo.com>
2025-03-31 10:40:48 +0300

    Creating a separate file class 'Animal' with Type and Sound and constructor, and in Program.cs creating a list of animals and iterating ine ah

---
+++ Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSound
+{
+    public class Animal
+    {
+        public string Type { get; set; }
+        public string Sound { get; set; }
+
+        public Animal(string type, string sound)
+        {
+            Type = type;
+            Sound = sound;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AnimalSound;
+
+namespace AnimalSound
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>
+            {
+                new Animal("Dog", "Bark"),
+                new Animal("Cat", "Meow"),
+                new Animal("Cow", "Moo")
+            };
+
+            foreach(var animal in animals)
+            {
+                Console.WriteLine($"{animal.Type} makes sound: {animal.Sound}");
+            }
+        }
+    }
+}
